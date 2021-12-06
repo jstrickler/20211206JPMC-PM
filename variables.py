6c97d5f7c34a0b9bb2f197c9d90dd0fa08d22282
John Strickler <jstrickler@gmail.com>
2021-12-06 21:40:53 +0000

    Monday 4:45

---
+++ variables.py
@@ -0,0 +1,36 @@
+
+a = 123      # int a = 123;    int a = new int(123);
+b = "456"
+c = 39
+d = None   # null value
+
+print(a + c)
+print(b)
+print(d)
+
+x = 5      #  assign 'x' to int object with value 5
+y = x      #  assign 'y' to same object as 'x'
+x = 10     #  re-assign 'x' to new int object with value 10
+print(x, y, type(x), type(y))
+
+customer_data_for_june_2020_without_texas = "whatevers..."
+
+#  a-z 0-9 _ A-Z
+
+class DogBreed:
+    pass
+
+a = 123
+b = "456"
+# print(a + b)
+print(a + 10)  # int add 10
+print(b + "10")   # str add "10"
+print(type(a), type(b), type(print), type(type))
+
+
+
+
+
+
+
+
---
+++ wombats.py
@@ -0,0 +1,11 @@
+print("Wombats are fun!")
+# print("They look cuddly but are not")
+print("They might bite you")  # line comment
+
+
+print('-' * 60)
+
+x = 5
+print("x: {}\n".format(x))
+
+
